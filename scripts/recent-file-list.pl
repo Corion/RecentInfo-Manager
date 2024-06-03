e5e22535d964cdc050a3f7709ebd779c8d451498
Max Maischein <corion@corion.net>
2024-06-03 20:16:00 +0200

    Allow filtering by mime type

--- scripts/recent-file-list.pl
+++ scripts/recent-file-list.pl
@@ -10,6 +10,7 @@ use PerlX::Maybe;
 GetOptions(
     'f|file=s' => \my $filename,
     'a|appname=s' => \my $appname,
+    't|mimetype=s' => \my $mimetype,
     'n|count=s' => \my $count,
 ) or pod2usage(2);
 
@@ -17,12 +18,18 @@ my $recent = RecentInfo::Manager->new(
     maybe filename => $filename,
 );
 
+sub mime_match( $type, $pattern ) {
+    $pattern =~ s/\*/.*/g;
+    $type =~ /$pattern/
+}
+
 my @res = map { $_->href } grep {
-    defined $appname ? $_->appname eq $appname
+      defined $appname ? $_->appname eq $appname
+    : defined $mimetype ? mime_match( $_->mime_type, $mimetype )
     : 1
 } $recent->entries->@*;
 
-if( $count ) {
+if( $count and @res ) {
     @res = splice @res, -$count;
 }
 say $_ for @res;
