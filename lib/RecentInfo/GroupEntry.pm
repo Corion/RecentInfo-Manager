ba42667c8b544786e2e6de49107466198fd996d2
Max Maischein <corion@corion.net>
2024-11-03 10:43:36 +0100

    Add license, repo, copyright documentation

--- lib/RecentInfo/GroupEntry.pm
+++ lib/RecentInfo/GroupEntry.pm
@@ -22,3 +22,31 @@ sub from_XML_fragment( $class, $frag ) {
 }
 
 1;
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/RecentInfo-Manager>.
+
+=head1 SUPPORT
+
+The public support forum of this module is L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via Github
+at L<https://github.com/Corion/RecentInfo-Manager/issues>
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2024-2024 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
+
